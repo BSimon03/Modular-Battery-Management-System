a40f9a0282b276be178fc891830744ab30f88d6b
tristanhorvath <tristan.horvath03@gmail.com>
2022-02-28 20:18:14 +0100

    update main.c

--- Software/BMS_Master/src/main.c
+++ Software/BMS_Master/src/main.c
@@ -29,24 +29,49 @@
 
 int main(void)
 {
-  timer_init_timer();
-  init_master();
+  timer_init_timer();   //initialize timers
+  init_master();    //initialize master
   uint8_t com_cnt=0;
-  enum TIMER_NR {
-   Wait,
+  enum COM_STATUS {
+   WAIT,
    SEND_TEMP_REQ,
    SEND_VOL_REQ,
    RECIEVE_TEMP,
    RECIEVE_VOL};
+   uint8_t com_status=WAIT;
+   uint16_t data_temp;
+
   while(1)
   {
     timer_add_time();
-    com_cnt=timer_get_timer(TIMER_COM);
-    if(com_cnt>=REQUEST_TIME)
+    com_cnt+=timer_get_timer(TIMER_COM);    //add timer to counter
+    if(com_cnt>=REQUEST_TIME)   //start requesting variables
     {
       com_cnt=0;
-      manch_init_send();
-      manch_send(REQ_TEMP_G);
+      switch(com_status)
+      {
+        case SEND_TEMP_REQ:   //send temperature request
+          manch_init_send();
+          manch_send(REQ_TEMP_G);
+          com_status=RECIEVE_TEMP;
+          break;
+        case RECIEVE_TEMP:    //initialize recieving
+          manch_init_receive();
+          com_status=WAIT;    
+        case WAIT:    //wait until done recieving
+        if(manch_receive(data_temp)==1)   //if recieving was successfull
+          {
+            com_status=SEND_VOL_REQ;
+            break;
+          }
+        else if(manch_receive(data_temp)==2)    //if an error ocurred, request again
+        {
+          com_status=RECIEVE_TEMP;
+          break;
+        }
+
+
+      }
     }
   }
 }
