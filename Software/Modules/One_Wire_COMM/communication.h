813f10c87e0cc4b3815f03931bf9e42a483f9869
tristanhorvath <tristan.horvath03@gmail.com>
2022-03-01 23:17:44 +0100

    .

--- Software/Modules/One_Wire_COMM/communication.h
+++ Software/Modules/One_Wire_COMM/communication.h
@@ -12,7 +12,7 @@
 #define COMM_H
 
 #define REQ_TEMP_G 0xD4AA   //global request for temperature 1 10 10101 0 0101010
-#define REQ_VOL_G 0xC633    //global request for voltage 1 10 00110 0 0110011
+#define REQ_VOLT_G 0xC633    //global request for voltage 1 10 00110 0 0110011
 #define COM_SLP_G 0xFF0F    //global command for sleep 1 11 1111 1 00001111
 #define COM_BLC_A 0xA600    //adressed command for balancing 1 01 00110 0 0000000 COM_BLC_A + adress = data
 
