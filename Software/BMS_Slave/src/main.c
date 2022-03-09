7247f97139d8b09976f2dead0a2b193ca1ec88d1
BSimon03 <90346844+BSimon03@users.noreply.github.com>
2022-03-09 23:06:45 +0100

    Significant modifications in communication
    Not done yet
    
    When to init receive or send?

--- Software/BMS_Slave/src/main.c
+++ Software/BMS_Slave/src/main.c
@@ -9,7 +9,7 @@
 /*  Author: Simon Ball                       */
 /*********************************************/
 
-//--------------CPU-FREQUENCY----------------------//
+//--------------CPU-FREQUENCY--------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //--Define CPU frequency, if not already defined in the platformio.ini or intellisense
 #ifndef F_CPU
 #define F_CPU 2000000L
@@ -32,7 +32,7 @@
 #error Invalid prescaler setting.
 #endif
 
-//--------------USED-HARDWARE----------------------//
+//--------------USED-HARDWARE--------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //--Define Microcontroller, if not already defined in the platform.ini or intellisense
 #ifndef __AVR_ATtiny261A__
 #define __AVR_ATtiny261A__
@@ -42,17 +42,17 @@
 #define BMS_SLAVE
 #endif
 
-//--------------PIN-DEFINITIONS--------------------//
+//--------------PIN-DEFINITIONS------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 #define DEBUG_DDR DDRB
 #define DEBUG_PORT PORTB
 #define DEBUG_PIN PINB5 // PCINT13
 
-//--------------SETTINGS---------------------------//
-#define MIN_VOLTAGE 0x04B0 // Minimum voltage for the battery
+//--------------SETTINGS-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define MIN_VOLTAGE 0x04B0 // Minimum voltage for the battery: ADC value = voltage x 400
 #define ADC_FILTER 1	   // Enable ADC filtering  0:OFF  1:ON
 #define ADC_SAMPLES 6	   // Averaging samples
 
-//--------------BALANCING--------------------------//
+//--------------BALANCING------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 #define BALANCING_DDR DDRB
 #define BALANCING_PORT PORTB
 #define BALANCING_PIN PINB4
@@ -61,14 +61,14 @@
 
 #define STOP_BALANCING() BALANCING_PORT &= ~(1 << BALANCING_PIN)
 
-//--------------LIBRARY-INCLUDES-------------------//
+//--------------LIBRARY-INCLUDES-----------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 #include <avr/io.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include <avr/eeprom.h>
 #include <avr/sleep.h>
 
-//--------------SOURCE-FILES-----------------------//
+//--------------SOURCE-FILES---------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // These are stored outside of the project folder, but will still be compiled
 #include "ADC.h"
 #include "communication.h"
@@ -84,7 +84,7 @@ enum ADC_STAT
 
 void bms_slave_init(void);
 
-//--------------MAIN-------------------------------//
+//--------------MAIN-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 int main(void)
 {
 	// Data received
@@ -115,7 +115,7 @@ int main(void)
 
 	uint8_t eeprom_stat = 0;
 	eeprom_stat = eeprom_read_byte(EEPROM_STATUS_ADR);
-	//--------------CALIBRATION------------------------//
+	//--------------CALIBRATION----------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 	if (!(eeprom_stat & EEPROM_CALIBRATED)) // if EEPROM not calibrated
 	{
 		while (!battery_voltage) // Measure SUPPLY voltage
@@ -164,7 +164,7 @@ int main(void)
 
 	while (1)
 	{
-		//--------------ADC--------------------------------//
+		//--------------ADC------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 		timer_add_time(); // executed after max 32ms
 
 		ADC_time = timer_get_timer(TIMER_ADC);
@@ -197,48 +197,71 @@ int main(void)
 				break;
 			}
 		}
-		//--------------PACKAGE-HANDLING-------------------//
-		if ((!(bot_received & ADDRESS_MASK)) && (bot_received & COM_BLC_A)) // checking if the current slave is addressed and command is balancing
+		//--------------BOT-PACKAGE-HANDLING-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		manch_init_receive(); // init receive from bot device
+
+		bot_send = 0; // reset bot_send... status
+		top_send = 0; // reset top_send... status
+
+		if (manch_receive(&bot_received)) // if received from bot
 		{
-			START_BALANCING();
-			//stat_led_orange();
+			if (bot_received & REQ_VOLT_G) /*BOT-X*/ // checking if battery voltage is requested
+			{
+				top_send = bot_received; // handout global command to next module
+				bot_send = battery_voltage;
+				bot_send |= (calc_parity(battery_voltage) << 15) | (1 << 16); // add parity to data
+			}
+			else if (bot_received & REQ_TEMP_G) /*BOT-X*/ // checking if battery temperature is requested
+			{
+				top_send = bot_received; // handout global command to next module
+				bot_send = battery_temperature;
+				bot_send |= (calc_parity(battery_temperature) << 15) | (1 << 16); // add parity to data
+			}
+			else if ((!(bot_received & ADDRESS_MASK)) && (bot_received & COM_BLC_A)) // checking if the current slave is addressed and command is balancing
+			{
+				START_BALANCING();
+				stat_led_orange();
+			}
+			else /*BOT-TOP*/ // current module not addressed... pass through addressed command
+			{
+				address_received = (uint8_t)bot_received & ADDRESS_MASK;
+				top_send = calc_data_bal(address_received - 1);
+			}
 		}
-		else if (bot_received & REQ_TEMP_G)
+		//--------------TOP-PACKAGE-HANDLING-------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+		manch_init_receive1();						  // init receive from top device
+		if (manch_receive(&top_received)) /*TOP-BOT*/ // if received from top
 		{
-			top_send = bot_received;
-			bot_send = battery_temperature;
-			bot_send |= (calc_parity(battery_temperature) << 14) | (1 << 15);
+			bot_send = top_received;
 		}
-		else if (bot_received & REQ_VOLT_G)
-		{
-			top_send = bot_received;
-		}
-		else
-		{
-			address_received = (uint8_t)bot_received & ADDRESS_MASK;
-			top_send = calc_data_bal(address_received - 1);
-		}
-		//--------------COMMUNICATION----------------------//
+
+		//--------------COMMUNICATION--------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 		if (COMM_time >= 1)
 		{
 			timer_clear_timer(TIMER_COMM);
 		}
 
-		manch_init_receive();			  // init receive from bot device
-		if (manch_receive(&bot_received)) // receive form bot
+		if (top_send) /*BOT-TOP*/ // if package for top ready
 		{
 			manch_init_send1();
 			manch_send1(top_send);
 		}
 
-		manch_init_receive1();			   // init receive from top device
-		if (manch_receive1(&top_received)) // receive form top
+		if (bot_send) /*X-BOT*/ // if package for bot ready
 		{
 			manch_init_send();
 			manch_send(bot_send);
 		}
 
-		//--------------BALANCING-TIMING---------------------//
+		// package received from top gets immediately send further down, no collision is expected
+		manch_init_receive1();						   // init receive from top device
+		if (manch_receive1(&top_received)) /*TOP-BOT*/ // receive from top
+		{
+			manch_init_send();
+			manch_send(top_received);
+		}
+
+		//--------------BALANCING-TIMING-------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 		if (BALANCE_time >= 10000) // Balancing Timeout of 10 000 ms
 		{
 			STOP_BALANCING();
