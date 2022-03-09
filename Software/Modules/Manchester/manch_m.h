7e961bf8f5c0d4b5c2ed8bac77bcd68410db0a45
BSimon03 <90346844+BSimon03@users.noreply.github.com>
2022-03-09 09:18:53 +0100

    description of fuctions reannotated

--- Software/Modules/Manchester/manch_m.h
+++ Software/Modules/Manchester/manch_m.h
@@ -70,35 +70,41 @@
 #endif // MANCHESTER1
 
 //=========== prototypen =======================================
-void manch_init_send(void);
 // initialisieren zum senden nach "unten"
 // vor jedem senden aufrufen!
-void manch_send(uint16_t data);
-// copiert daten von data und startet das senden
+void manch_init_send(void);
 
-void manch_init_receive();
+// copiert daten von data und startet das senden
+void manch_send(uint16_t data);
+
 // initialisieren zum empfang von "unten"
 // vor jedem empfangen aufrufen
-uint8_t manch_receive(uint16_t *data);
+void manch_init_receive();
+
 // return 0: wartet noch auf daten
 // return 1: daten empfangen, in *data copiert
 // return 2: fehlerhafter empfang, empfang abgebrochen, muss neu gestartet werden
+uint8_t manch_receive(uint16_t *data);
 
 #ifdef MANCHESTER1
-void manch_init_send1(void);
+
 // zum senden nach "oben"
 // initialisiert nur das sendeport.
 // danach manch_init_send() aufrufen!
-void manch_send1(uint16_t data);
+void manch_init_send1(void);
+
 // copiert nur die sendedaten für senden nach "oben",
 // danach mach_send() zum starten aufrufen!
-void manch_init_receive1();
+void manch_send1(uint16_t data);
+
 // initialisieren zum empfang von "oben"
 // vor jedem empfangen aufrufen
-uint8_t manch_receive1(uint16_t *data);
+void manch_init_receive1();
+
 // return 0: wartet noch auf daten
 // return 1: daten empfangen, in *data copiert
 // return 2: fehlerhafter empfang, empfang abgebrochen, muss neu gestartet werden
+uint8_t manch_receive1(uint16_t *data);
 
 #endif // MANCHESTER1
 #endif // MANCH_H
